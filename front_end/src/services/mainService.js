60492463673da7568966136677ebae6c73b7421b
Masih <masih.saldanha@gmail.com>
2022-11-26 18:29:17 -0300

    Fix: Fix frontend request

--- front_end/src/services/mainService.js
+++ front_end/src/services/mainService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const site = "http://ec2-52-90-242-30.compute-1.amazonaws.com/api";
+// const site = process.env.REACT_APP_BASE_URL;
 
 function getBalance(config) {
     return axios.get(`${site}/transaction/balance`, config);
